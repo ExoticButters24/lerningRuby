d1390ce7957dc50e56f021bb650dbba80d124d6a
Isai P <piedraisai24@gmail.com>
2022-12-08 18:16:15 -0600

    IO primer commit

---
+++ 3.Input_Output/IO.rb
@@ -0,0 +1,14 @@
+#Entrada y salida de datos
+#Input and Output
+
+#Indicación al usuario 
+print "Indique su nombre: "
+
+#Función para obtener datos de consola y almacenarlos en la variable
+name = gets
+
+#Método para imprimir datos sin salto de línea
+print "Hola #{name}"
+
+#Método para imprimir datos con un salto de línea al final
+puts "Hola #{name}"
